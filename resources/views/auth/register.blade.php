7fdf7d866c626f741392ec7b8f0b26ae34be27fc
mukki00 <mohamedmukkicolombo@gmail.com>
2021-12-16 23:44:40 +0530

    registered files changed according to keycode value

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -73,6 +73,13 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+                        <div class="form-group row">
+                            <label for="keycode" class="col-md-4 col-form-label text-md-right">{{ __('Key Code') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="keycode" type="password" class="form-control" name="keycode" required>
+                            </div>
+                        </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">

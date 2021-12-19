225b44d71d9958f92f1cb1fc548496ccd3cde26f
mukki00 <mohamedmukkicolombo@gmail.com>
2021-12-19 20:34:51 +0530

    Verions 1.0.1 is issude correcting bugs

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0.0
+      <b>Version</b> 1.0.1
     </div>
     <strong>Copyright &copy; 2021-2022 <a href="https://ceylonsuper.com/">Ceylon Super</a>.</strong> All rights
     reserved.
